a1b6cdeea92b526c007517437e06cfb61dca8596
Yana Chorna <johanna1988@ukr.net>
2022-08-05 12:23:06 +0300

    Removed comments, fixed mistake

--- src/main/java/generics/Generics.java
+++ src/main/java/generics/Generics.java
@@ -3,12 +3,11 @@ package main.java.generics;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
+
 public class Generics {
     public static void main(String[] args) {
-            ///////////////// Java 5 ////////////////////
             List animals = new ArrayList();
-            Animal ourAnimal = new Animal;
+            Animal ourAnimal = new Animal();
             animals.add("cat");
             animals.add("dog");
             animals.add("frog");
@@ -17,16 +16,13 @@ public class Generics {
             String animal = (String) animals.get(1);
             System.out.println(animal);
 
-            /////////////////////// generics /////////////////////
             List<String> animals2 = new ArrayList<String>();
             animals2.add("cat");
             animals2.add("dog");
             animals2.add("frog");
-            animals2.add(ourAnimal);
 
             String animal2 = animals2.get(1);
 
-            ////////////////// Java 7 //////////////////
             List<String> animals3 = new ArrayList<>();
         }
     }
@@ -34,4 +30,3 @@ public class Generics {
 class Animal {
 
 }
-**/
